21677b2861abff441bf221f123edab98acf816df
Guillermo López <willyaranda@gmail.com>
2011-04-04 22:29:02 +0200

    Siempre escribo lenght mal... length quiero decir

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import ugettext_lazy as _
+
+class Usuario(models.Model):
+    nombre = models.CharField(_("Nombre"), max_length=50, blank=True)
+    nick = models.CharField(_("Nick"), max_length=50, blank=True)
+    slug = models.SlugField(_("Nombre corto"), max_length=50)
+    correo = models.EmailField(_("Correo"))
+    
+    def __unicode__(self):
+        return u'%s' % (self.name)
+    
