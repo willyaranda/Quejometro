317e10f4c4c6afbfd542418bdc56f114e8e659a2
Guillermo López <willyaranda@gmail.com>
2011-05-07 02:11:52 +0200

    Oops, change in STATIC_ROOT  and in template base.html

--- settings.py
+++ settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = SITE_URL+'/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/public/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
